5a139952df73913f34c64c1ab2656a7171e8fef8
Laurel Schmidt <laurel.schmidt@umconnect.umt.edu>
2023-11-11 21:49:38 -0700

    Hw11

---
+++ (New) Homework 11/Sketchgame11.js
@@ -0,0 +1,119 @@
+//character var
+var characterX = 20;
+var characterY = 20;
+
+//key codes
+var w = 87;
+var s = 83;
+var a = 65;
+var d = 68;
+
+//shape
+var shapeX = 30;
+var shapeY = 50;
+var shapeXSpeed;
+var shapeYSpeed;
+
+//mouse click
+var mouseShapeX;
+var mouseShapeY;
+
+
+
+function setup() {
+    createCanvas(500,700);
+ 
+}
+function draw() {
+    colorMode(HSB);
+    background(245,81,33);
+    describe('a canvas with dark blue background')
+}
+
+    function draw () 
+    {
+        colorMode(HSB);
+        background(245,81,33);
+
+        //rectangle 1 org (30,40,50,60) (160,350,90,170)
+        fill(0,0,0);
+        rect(260,50,290,70);
+
+        //rect 2 (260,50,290,70)
+        fill(0,0,0);
+        rect(260,250,290,70);
+
+        //rect 3 vertical
+        fill(0,0,0);
+        rect(260,50,90,170);
+
+
+        //rect 4
+        fill(0,0,0);
+        rect(60,50,290,70);
+
+        //rect 5 vertical 2 long
+        fill(0,0,0);
+        rect(60,50,90,570);
+
+        //rect 6 
+        fill(0,0,0);
+        rect(60,350,290,70);
+
+        //rect 7 block
+        fill(0,0,0);
+        rect(460,150,90,170);
+
+        fill(0,0,0);
+        rect(260,450,290,270);
+
+        //Exit text
+        fill(0,0,100);
+        textSize(19);
+        text("Exit",450, 140)
+
+        //character
+        fill(44,100,89);
+        circle(characterX,characterY,20);
+
+        //keys
+        if(keyIsDown(w))
+        {
+            characterY -= 10;
+        }
+        if(keyIsDown(s))
+        {
+            characterY += 10;
+        }
+        if(keyIsDown(a))
+        {
+            characterX -=10;
+        }
+        if(keyIsDown(d))
+        {
+            characterX += 10;
+        }
+
+        //you win
+        if(characterX > 450 && characterY > width -450)
+        {
+            fill(0,0,100);
+            stroke(10);
+            textSize(30);
+            text("You Win!", width/2-190, height/2-250);
+        }
+
+          //mouseclick
+          fill(0,0,100);
+          circle(mouseShapeX, mouseShapeY,25); 
+  
+
+
+    }
+   
+    function mouseClicked()
+    {
+        mouseShapeX = mouseX;
+        mouseShapeY = mouseY;
+    }
+
